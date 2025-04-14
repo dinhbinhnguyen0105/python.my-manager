f9ecf1d532c06384693cad51961409c0c9c8fa19
DinhBinh Nguyen <dinhbinhnguyen@192.168.1.11.non-exists.ptr.local>
2025-04-14 20:56:36 +0700

    from macmini

---
+++ src/constants.py
@@ -0,0 +1,296 @@
+import os
+PATH_RE_DB = os.path.join("src", "data", "real_estate.db")
+RE_CONNECTION = "re_connection"
+PATH_USER_DB = os.path.join("src", "data", "user.db")
+USER_CONNECTION = "user_connection"
+
+TABLE_RE = "table_re"
+TABLE_USER = "table_user"
+
+TABLE_RE_SETTINGS_STATUSES = "table_re_settings_statuses"
+TABLE_RE_SETTINGS_PROVINCES = "table_re_settings_provinces"
+TABLE_RE_SETTINGS_DISTRICTS = "table_re_settings_districts"
+TABLE_RE_SETTINGS_WARDS = "table_re_settings_wards"
+TABLE_RE_SETTINGS_OPTIONS = "table_re_settings_options"
+TABLE_RE_SETTINGS_CATEGORIES = "table_re_settings_categories"
+TABLE_RE_SETTINGS_BUILDING_LINES = "table_re_settings_building_lines"
+TABLE_RE_SETTINGS_FURNITURES = "table_re_settings_furnitures"
+TABLE_RE_SETTINGS_LEGALS = "table_re_settings_legals"
+TABLE_RE_SETTINGS_IMG_DIRS = "table_re_settings_image_directories"
+
+TABLE_RE_SETTINGS_TITLE = "table_re_settings_title"
+TABLE_RE_SETTINGS_DESCRIPTION = "table_re_settings_description"
+
+TABLE_USER_SETTINGS_UDD = "table_user_settings_udd"
+TABLE_USER_SETTINGS_PROXY = "table_user_settings_proxy"
+
+ICONS = [
+    "🌼",
+    "🌸",
+    "🌺",
+    "🏵️",
+    "🌻",
+    "🌷",
+    "🌹",
+    "🥀",
+    "💐",
+    "🌾",
+    "🎋",
+    "☘",
+    "🍀",
+    "🍃",
+    "🍂",
+    "🍁",
+    "🌱",
+    "🌿",
+    "🎍",
+    "🌵",
+    "🌴",
+    "🌳",
+    "🎄",
+    "🍄",
+    "🌎",
+    "🌍",
+    "🌏",
+    "🌜",
+    "🌛",
+    "🌕",
+    "🌖",
+    "🌗",
+    "🌘",
+    "🌑",
+    "🌒",
+    "🌓",
+    "🌔",
+    "🌚",
+    "🌝",
+    "🌙",
+    "💫",
+    "⭐",
+    "🌟",
+    "✨",
+    "⚡",
+    "🔥",
+    "💥",
+    "☄️",
+    "🌞",
+    "☀️",
+    "🌤️",
+    "⛅",
+    "🌥️",
+    "🌦️",
+    "☁️",
+    "🌧️",
+    "⛈️",
+    "🌩️",
+    "🌨️",
+    "🌈",
+    "💧",
+    "💦",
+    "☂️",
+    "☔",
+    "🌊",
+    "🌫",
+    "🌪",
+    "💨",
+    "❄",
+    "🌬",
+    "⛄",
+    "☃️",
+    "♥️",
+    "❤️",
+    "💛",
+    "💚",
+    "💙",
+    "💜",
+    "🖤",
+    "💖",
+    "💝",
+    "💔",
+    "❣️",
+    "💕",
+    "💞",
+    "💓",
+    "💗",
+    "💘",
+    "💟",
+    "💌",
+    "💋",
+    "👄",
+    "💄",
+    "💍",
+    "📿",
+    "🎁",
+    "👙",
+    "👗",
+    "👚",
+    "👕",
+    "👘",
+    "️🎽",
+    "👘",
+    "👖",
+    "👠",
+    "👡",
+    "👢",
+    "👟",
+    "👞",
+    "👒",
+    "🎩",
+    "🎓",
+    "👑",
+    "⛑️",
+    "👓",
+    "🕶️",
+    "🌂",
+    "👛",
+    "👝",
+    "👜",
+    "💼",
+    "🎒",
+    "🛍️",
+    "️🛒",
+    "️🎭",
+    "️🎦",
+    "️🎨",
+    "️🤹",
+    "️🎊",
+    "️🎉",
+    "️🎈",
+    "️🎧",
+    "️🎷",
+    "️🎺",
+    "️🎸",
+    "️🎻",
+    "️🥁",
+    "️🎹",
+    "️🎤",
+    "️🎵",
+    "️🎶",
+    "️🎼",
+    "️⚽",
+    "️🏀",
+    "️🏈",
+    "️⚾",
+    "️🏐",
+    "️🏉",
+    "️🎱",
+    "️🎾",
+    "️🏸",
+    "️🏓",
+    "️🏏",
+    "️🏑",
+    "️🏒",
+    "️🥅",
+    "️⛸️",
+    "️🎿",
+    "️🥊",
+    "️🥋",
+    "️⛳",
+    "️🎳",
+    "️🏹",
+    "️🎣",
+    "️🎯",
+    "🚵",
+    "️🎖️",
+    "️🏅",
+    "️🥇",
+    "️🥈",
+    "️🥉",
+    "️🏆",
+]
+RE_SETTING_STATUSES = [
+    {"label_vi": "sẵn có", "label_en": "available", "value": "available"},
+    {"label_vi": "không sẵn có", "label_en": "unavailable", "value": "unavailable"},
+    {"label_vi": "đã ngừng", "label_en": "discontinued", "value": "discontinued"},
+    {"label_vi": "sắp có", "label_en": "coming soon", "value": "coming_soon"},
+]
+RE_SETTING_PROVINCES = [
+    {"label_vi": "lâm đồng", "label_en": "lam dong", "value": "lam_dong"},
+]
+RE_SETTING_DISTRICTS = [
+    {"label_vi": "đà lạt", "label_en": "da lat", "value": "da_lat"},
+]
+RE_SETTING_WARDS = [
+    {"label_vi": "phường 1", "label_en": "ward 1", "value": "1"},
+    {"label_vi": "phường 2", "label_en": "ward 2", "value": "2"},
+    {"label_vi": "phường 3", "label_en": "ward 3", "value": "3"},
+    {"label_vi": "phường 4", "label_en": "ward 4", "value": "4"},
+    {"label_vi": "phường 5", "label_en": "ward 5", "value": "5"},
+    {"label_vi": "phường 6", "label_en": "ward 6", "value": "6"},
+    {"label_vi": "phường 7", "label_en": "ward 7", "value": "7"},
+    {"label_vi": "phường 8", "label_en": "ward 8", "value": "8"},
+    {"label_vi": "phường 9", "label_en": "ward 9", "value": "9"},
+    {"label_vi": "phường 10", "label_en": "ward 10", "value": "10"},
+    {"label_vi": "phường 11", "label_en": "ward 11", "value": "11"},
+    {"label_vi": "phường 12", "label_en": "ward 12", "value": "12"},
+    {
+        "label_vi": "xã trạm hành",
+        "label_en": "ward tram hanh",
+        "value": "tram_hanh",
+    },
+    {"label_vi": "xã tà nung", "label_en": "ward ta nung", "value": "ta_nung"},
+    {
+        "label_vi": "xã xuân trường",
+        "label_en": "ward xuan truong",
+        "value": "xuan_truong",
+    },
+    {"label_vi": "xã xuân thọ", "label_en": "ward xuan tho", "value": "xuan_tho"},
+]
+RE_SETTING_OPTIONS = [
+    {"label_vi": "bán", "label_en": "sell", "value": "sell"},
+    {"label_vi": "cho thuê", "label_en": "rent", "value": "rent"},
+    {"label_vi": "sang nhượng", "label_en": "assignment", "value": "assignment"},
+]
+RE_SETTING_CATEGORIES = [
+    {"label_vi": "nhà phố", "label_en": "private house", "value": "private_house"},
+    {"label_vi": "nhà mặt tiền", "label_en": "shop house", "value": "shop_house"},
+    {"label_vi": "biệt thự", "label_en": "villa", "value": "villa"},
+    {"label_vi": "đất nền", "label_en": "land", "value": "land"},
+    {"label_vi": "căn hộ/ chung cư", "label_en": "apartment", "value": "apartment"},
+    {"label_vi": "homestay", "label_en": "homestay", "value": "homestay"},
+    {"label_vi": "khách sạn", "label_en": "hotel", "value": "hotel"},
+    {"label_vi": "kho/bãi", "label_en": "workshop", "value": "workshop"},
+    {"label_vi": "MBKD", "label_en": "retail space", "value": "retail_space"},
+    {
+        "label_vi": "coffee house",
+        "label_en": "coffee house",
+        "value": "coffee_house",
+    },
+    {"label_vi": "nhà hàng", "label_en": "restaurant", "value": "restaurant"},
+]
+RE_SETTING_BUILDING_LINES = [
+    {"label_vi": "đường xe hơi", "label_en": "big road", "value": "big_road"},
+    {"label_vi": "đường xe máy", "label_en": "small road", "value": "small_road"},
+]
+RE_SETTING_LEGALS = [
+    {"label_vi": "giấy tờ tay", "label_en": "none", "value": "none"},
+    {"label_vi": "sổ nông nghiệp chung", "label_en": "snnc", "value": "snnc"},
+    {
+        "label_vi": "sổ nông nghiệp phân quyền",
+        "label_en": "snnpq",
+        "value": "snnpq",
+    },
+    {"label_vi": "sổ nông nghiệp riêng", "label_en": "snnr", "value": "snnr"},
+    {"label_vi": "sổ xây dựng chung", "label_en": "sxdc", "value": "sxdc"},
+    {"label_vi": "sổ xây dựng phân quyền", "label_en": "sxdpq", "value": "sxdpq"},
+    {"label_vi": "sổ xây dựng riêng", "label_en": "sxdr", "value": "sxdr"},
+]
+RE_SETTING_FURNITURES = [
+    {"label_vi": "không nội thất", "label_en": "none", "value": "none"},
+    {"label_vi": "nội thất cơ bản", "label_en": "basic", "value": "basic"},
+    {"label_vi": "đầy đủ nội thất", "label_en": "full", "value": "full"},
+]
+RE_SETTING_IMG_DIRS = [
+    {"value": os.path.join("repositories", "products", "re")}]
+RE_SETTING_TEMPLATE_TITLES = [
+    {
+        "tid": "T.T.default", "option_id": 1,
+        "value": "[<option>] <icon><icon> cần <option> <category> <price> <unit>, <ward>, <district>, <province> <icon><icon>"
+    }
+]
+RE_SETTING_TEMPLATE_DESCRIPTIONS = [
+    {
+        "tid": "T.D.default", "option_id": 1,
+        "value": "ID: <PID>\n🗺 Vị trí: đường <street>, <ward>, <district>\n📏 Diện tích: <area>\n🏗 Kết cấu: <structure>\n🛌 Công năng: <function>\n📺 Nội thất: <furniture>\n🚗 Lộ giới: <building_line>\n📜 Pháp lý: <legal>\n<icon><icon> Mô tả:\n<description>\n------------\n💵 Giá: <price><unit>- Thương lượng chính chủ\n\n☎ Liên hệ: 0375.155.525 - Mr. Bình\n🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺\n🌺Ký gửi mua, bán - cho thuê, thuê bất động sản xin liên hệ 0375.155.525 - Mr. Bình🌺\n🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺🌺",
+    }
+]
---
+++ src/models/re_database.py
@@ -0,0 +1,259 @@
+import logging
+import sys
+
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery, QSqlRecord
+from src.constants import (
+    RE_CONNECTION,
+    PATH_RE_DB,
+    TABLE_RE,
+    TABLE_RE_SETTINGS_STATUSES,
+    TABLE_RE_SETTINGS_PROVINCES,
+    TABLE_RE_SETTINGS_DISTRICTS,
+    TABLE_RE_SETTINGS_WARDS,
+    TABLE_RE_SETTINGS_OPTIONS,
+    TABLE_RE_SETTINGS_CATEGORIES,
+    TABLE_RE_SETTINGS_BUILDING_LINES,
+    TABLE_RE_SETTINGS_FURNITURES,
+    TABLE_RE_SETTINGS_LEGALS,
+    TABLE_RE_SETTINGS_TITLE,
+    TABLE_RE_SETTINGS_DESCRIPTION,
+    TABLE_RE_SETTINGS_IMG_DIRS,
+    RE_SETTING_STATUSES,
+    RE_SETTING_PROVINCES,
+    RE_SETTING_DISTRICTS,
+    RE_SETTING_WARDS,
+    RE_SETTING_OPTIONS,
+    RE_SETTING_CATEGORIES,
+    RE_SETTING_BUILDING_LINES,
+    RE_SETTING_LEGALS,
+    RE_SETTING_FURNITURES,
+    RE_SETTING_IMG_DIRS,
+    RE_SETTING_TEMPLATE_TITLES,
+    RE_SETTING_TEMPLATE_DESCRIPTIONS,
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s')
+handler = logging.StreamHandler(sys.stderr)
+handler.setFormatter(formatter)
+if not logger.hasHandlers():
+    logger.addHandler(handler)
+
+base_settings_tables_data = {
+    TABLE_RE_SETTINGS_STATUSES: RE_SETTING_STATUSES,
+    TABLE_RE_SETTINGS_PROVINCES: RE_SETTING_PROVINCES,
+    TABLE_RE_SETTINGS_DISTRICTS: RE_SETTING_DISTRICTS,
+    TABLE_RE_SETTINGS_WARDS: RE_SETTING_WARDS,
+    TABLE_RE_SETTINGS_OPTIONS: RE_SETTING_OPTIONS,
+    TABLE_RE_SETTINGS_CATEGORIES: RE_SETTING_CATEGORIES,
+    TABLE_RE_SETTINGS_BUILDING_LINES: RE_SETTING_BUILDING_LINES,
+    TABLE_RE_SETTINGS_FURNITURES: RE_SETTING_FURNITURES,
+    TABLE_RE_SETTINGS_LEGALS: RE_SETTING_LEGALS,
+    TABLE_RE_SETTINGS_IMG_DIRS: RE_SETTING_IMG_DIRS,
+}
+templates_tables_data = {
+    TABLE_RE_SETTINGS_TITLE: RE_SETTING_TEMPLATE_TITLES,
+    TABLE_RE_SETTINGS_DESCRIPTION: RE_SETTING_TEMPLATE_DESCRIPTIONS,
+}
+
+
+def initialize_re_db():
+    db = QSqlDatabase.addDatabase("QSQLITE", RE_CONNECTION)
+    db.setDatabaseName(PATH_RE_DB)
+    if not db.open():
+        logger.error(f"Error opening database: {db.lastError().text()}")
+        return False
+    query = QSqlQuery(db)
+    query.exec("PRAGMA foreign_keys = ON;")
+    try:
+        if not _create_tables(db):
+            return False
+        if not _seed_initial_data(db):
+            return False
+        return True
+    except Exception as e:
+        if db.isOpen():
+            db.rollback()
+        logger.error(
+            f"Database initialization failed: {str(e)}")
+        return False
+
+
+def _create_tables(db):
+    try:
+        if not db.transaction():
+            logger.error("Could not start transaction.")
+            return False
+
+        for table_name in base_settings_tables_data:
+            base_settings_sql = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    label_vi TEXT,
+    label_en TEXT,
+    value TEXT UNIQUE NOT NULL,
+    updated_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
+    created_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now'))
+    )
+    """
+            if not _create_table(table_name, db, base_settings_sql):
+                db.rollback()
+                return False
+
+        for table_name in templates_tables_data:
+            template_sql = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tid TEXT UNIQUE,
+        option_id INTEGER,
+        value TEXT,
+        updated_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
+        created_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
+        FOREIGN KEY (option_id) REFERENCES {TABLE_RE_SETTINGS_OPTIONS}(id)
+    )
+    """
+            if not _create_table(table_name, db, template_sql):
+                db.rollback()
+                return False
+        image_dir_sql = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_RE_SETTINGS_IMG_DIRS} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        value TEXT UNIQUE NOT NULL,
+        is_selected INTEGER,
+        updated_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
+        created_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now'))
+    )
+    """
+        if not _create_table(TABLE_RE_SETTINGS_IMG_DIRS, db, image_dir_sql):
+            db.rollback()
+            return False
+
+        product_sql = f"""
+    CREATE TABLE IF NOT EXISTS {TABLE_RE} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        pid TEXT UNIQUE NOT NULL,
+        status_id INTEGER,
+        option_id INTEGER,
+        ward_id INTEGER,
+        street TEXT,
+        category_id INTEGER,
+        area REAL,
+        price REAL,
+        legal_id INTEGER,
+        province_id INTEGER,
+        district_id INTEGER,
+        structure REAL,
+        function TEXT,
+        building_line_id INTEGER,
+        furniture_id INTEGER,
+        description TEXT,
+        created_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
+        updated_at TEXT DEFAULT (strftime('%Y-%m-%d %H:%M:%S', 'now')),
+        FOREIGN KEY (status_id) REFERENCES {TABLE_RE_SETTINGS_STATUSES}(id),
+        FOREIGN KEY (province_id) REFERENCES {TABLE_RE_SETTINGS_PROVINCES}(id),
+        FOREIGN KEY (district_id) REFERENCES {TABLE_RE_SETTINGS_DISTRICTS}(id),
+        FOREIGN KEY (ward_id) REFERENCES {TABLE_RE_SETTINGS_WARDS}(id),
+        FOREIGN KEY (option_id) REFERENCES {TABLE_RE_SETTINGS_OPTIONS}(id),
+        FOREIGN KEY (category_id) REFERENCES {TABLE_RE_SETTINGS_CATEGORIES}(id),
+        FOREIGN KEY (building_line_id) REFERENCES {TABLE_RE_SETTINGS_BUILDING_LINES}(id),
+        FOREIGN KEY (furniture_id) REFERENCES {TABLE_RE_SETTINGS_FURNITURES}(id),
+        FOREIGN KEY (legal_id) REFERENCES {TABLE_RE_SETTINGS_LEGALS}(id)
+    )
+    """
+        if not _create_table(TABLE_RE, db, product_sql):
+            db.rollback()
+            return False
+        if not db.commit():
+            logger.error("Commit failed")
+            return False
+        return True
+    except Exception as e:
+        if db.isOpen():
+            db.rollback()
+        logger.error(
+            f"Database creating failed: {str(e)}")
+        return False
+
+
+def _seed_initial_data(db: QSqlDatabase):
+    for table_name, data_list in base_settings_tables_data.items():
+        if not _seed_data(db, table_name, data_list):
+            return False
+    for table_name, data_list in templates_tables_data.items():
+        if not _seed_data(db, table_name, data_list):
+            return False
+    return True
+
+
+def _seed_data(db: QSqlDatabase, table_name: str, payload: list):
+    if not db.transaction():
+        logger.error(
+            f"Failed to start transaction for seeding table '{table_name}'.")
+        return False
+    query = QSqlQuery(db)
+
+    # Lấy thông tin cột của bảng
+    record = db.record(table_name)
+    columns = [record.fieldName(i) for i in range(record.count())]
+
+    # Loại bỏ cột id (autoincrement) nếu có
+    if 'id' in columns:
+        columns.remove('id')
+    if 'created_at' in columns:
+        columns.remove("created_at")
+    if 'updated_at' in columns:
+        columns.remove("updated_at")
+
+    # Tạo placeholder cho các giá trị
+    placeholders = ', '.join(['?'] * len(columns))
+    columns_str = ', '.join(columns)
+    sql = f"INSERT OR IGNORE INTO {table_name} ({columns_str}) VALUES ({placeholders})"
+    query.prepare(sql)
+    for row_data in payload:
+        if isinstance(row_data, dict):
+            # Đảm bảo thứ tự giá trị tương ứng với thứ tự cột
+            values = [row_data.get(col) for col in columns]
+            for value in values:
+                query.addBindValue(value)
+            if not query.exec():
+                logger.error(
+                    f"Error seeding data into '{table_name}': {query.lastError().text()} - Data: {row_data}")
+                db.rollback()
+                return False
+        elif isinstance(row_data, (list, tuple)):
+            if len(row_data) != len(columns):
+                logger.error(
+                    f"Number of values in row does not match number of columns in '{table_name}': {row_data} vs {columns}")
+                db.rollback()
+                return False
+            for value in row_data:
+                query.addBindValue(value)
+            if not query.exec():
+                logger.error(
+                    f"Error seeding data into '{table_name}': {query.lastError().text()} - Data: {row_data}")
+                db.rollback()
+                return False
+        else:
+            logger.error(
+                f"Invalid data format for seeding '{table_name}': {row_data}")
+            db.rollback()
+            return False
+    query.clear()
+    # Chuyển commit ra khỏi vòng lặp dữ liệu
+    if not db.commit():
+        logger.error(
+            f"Failed to commit transaction for seeding table '{table_name}'.")
+        return False
+    return True
+
+
+def _create_table(table_name, db, sql):
+    query = QSqlQuery(db)
+    if not query.exec(sql):
+        logger.error(
+            f"Error creating table '{table_name}': {query.lastError().text()}"
+        )
+        return False
+    return True
